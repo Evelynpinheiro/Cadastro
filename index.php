5f2f44809825964f4cc43313a86735d82ad47e7a
Evelynpinheiro <58746094+Evelynpinheiro@users.noreply.github.com>
2020-03-12 09:36:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
 	<body>
 		<h1>Cadastrar Usuário</h1>
 		<form method="POST" action="processo.php">
-		</form>
+
+		<label>Nome:</label>
+        <input type="text" name="nome" placeholder="Digite seu nome"><br><br>
+
+        <label>Endereço:</label>
+        <input type="text" name="endereco" placeholder="Digite seu endereço"><br><br>
+
+        <label>Email:</label>
+        <input type="email" name="email" placeholder="Digite seu email"><br><br>
+
+        <label>Telefone:</label>
+        <input type="int" name="telefone" placeholder="Digite seu telefone"><br><br>
+
+        </form>
 	</body>	
-</html>
+</html
